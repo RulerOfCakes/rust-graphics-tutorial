b6ebd933b9768aa1cc1f0e8a10cbc50fe3bfbbff
RulerOfCakes <fragmentbombs@gmail.com>
2024-11-05 22:20:46 +0900

    feat: basic 3d cellular automata

--- w1/libs/simulation/src/lib.rs
+++ w1/libs/simulation/src/lib.rs
@@ -1,5 +1,149 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashSet;
+
+pub enum Neighborhood {
+    Moore,
+    VonNeuman,
+}
+
+impl Neighborhood {
+    fn get_neighbors(&self, x: u32, y: u32, z: u32) -> Vec<(u32, u32, u32)> {
+        match self {
+            Neighborhood::Moore => {
+                let mut neighbors = vec![];
+                for i in -1i32..2 {
+                    for j in -1i32..2 {
+                        for k in -1i32..2 {
+                            if i == 0 && j == 0 && k == 0 {
+                                continue;
+                            }
+                            let x = x.saturating_add_signed(i);
+                            let y = y.saturating_add_signed(j);
+                            let z = z.saturating_add_signed(k);
+                            neighbors.push((x, y, z));
+                        }
+                    }
+                }
+                neighbors
+            }
+            Neighborhood::VonNeuman => {
+                let mut neighbors = vec![];
+                for i in -1i32..2 {
+                    for j in -1i32..2 {
+                        for k in -1i32..2 {
+                            if i == 0 && j == 0 && k == 0 {
+                                continue;
+                            }
+                            if i.abs() + j.abs() + k.abs() > 1 {
+                                continue;
+                            }
+                            let x = x.saturating_add_signed(i);
+                            let y = y.saturating_add_signed(j);
+                            let z = z.saturating_add_signed(k);
+                            neighbors.push((x, y, z));
+                        }
+                    }
+                }
+                neighbors
+            }
+        }
+    }
+}
+
+pub struct Ruleset {
+    alive: HashSet<u8>,
+    born: HashSet<u8>,
+    health: u8,
+    neighborhood: Neighborhood,
+    x: u32,
+    y: u32,
+    z: u32,
+}
+
+impl Ruleset {
+    pub fn new(
+        alive: Vec<u8>,
+        born: Vec<u8>,
+        health: u8,
+        neighborhood: Neighborhood,
+        x: u32,
+        y: u32,
+        z: u32,
+    ) -> Ruleset {
+        Ruleset {
+            alive: alive.iter().cloned().collect(),
+            born: born.iter().cloned().collect(),
+            health,
+            neighborhood,
+            x,
+            y,
+            z,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Cell(u8);
+
+impl Cell {
+    fn is_alive(&self) -> bool {
+        self.0 > 0
+    }
+}
+
+pub struct CellularAutomaton {
+    cells: Vec<Vec<Vec<Cell>>>,
+    rule: Ruleset,
+}
+
+impl CellularAutomaton {
+    pub fn new(rule: Ruleset) -> CellularAutomaton {
+        let cells = vec![vec![vec![Cell(0); rule.z as usize]; rule.y as usize]; rule.x as usize];
+        CellularAutomaton { cells, rule }
+    }
+    fn get_neighbors(&self, x: u32, y: u32, z: u32) -> Vec<Cell> {
+        let coords = self.rule.neighborhood.get_neighbors(x, y, z);
+
+        coords
+            .iter()
+            .filter(|(x, y, z)| x < &self.rule.x && y < &self.rule.y && z < &self.rule.z)
+            .map(|(x, y, z)| self.cells[*x as usize][*y as usize][*z as usize])
+            .filter(|cell| cell.is_alive())
+            .collect()
+    }
+    pub fn step(mut self) -> Self {
+        let mut new_cells = vec![
+            vec![vec![Cell(0); self.rule.z as usize]; self.rule.y as usize];
+            self.rule.x as usize
+        ];
+        for x in 0..self.cells.len() {
+            for y in 0..self.cells[x].len() {
+                for z in 0..self.cells[x][y].len() {
+                    let cell = self.cells[x][y][z];
+                    // check if it is already dying
+                    if cell.is_alive() && cell.0 > 1 {
+                        new_cells[x][y][z] = Cell(cell.0 - 1);
+                        continue;
+                    }
+                    let neighbors = self.get_neighbors(x as u32, y as u32, z as u32);
+                    if cell.is_alive() {
+                        // check for alive condition
+                        if self.rule.alive.contains(&(neighbors.len() as u8)) {
+                            new_cells[x][y][z] = self.cells[x][y][z];
+                        } else {
+                            new_cells[x][y][z] = Cell(0);
+                        }
+                    } else {
+                        // check for born condition
+                        if self.rule.born.contains(&(neighbors.len() as u8)) {
+                            new_cells[x][y][z] = Cell(self.rule.health);
+                        }
+                    }
+                }
+            }
+        }
+        self.cells = new_cells;
+        self
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +151,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_step() {
+        let mut ca_445_m = CellularAutomaton::new(Ruleset {
+            alive: [4].iter().cloned().collect(),
+            born: [4].iter().cloned().collect(),
+            health: 5,
+            neighborhood: Neighborhood::Moore,
+            x: 10,
+            y: 10,
+            z: 10,
+        });
+
+        ca_445_m.cells[5][5][5] = Cell(5);
+        ca_445_m.cells[5][5][6] = Cell(5);
+        ca_445_m.cells[7][5][5] = Cell(5);
+        ca_445_m.cells[7][5][6] = Cell(5);
+
+        let ca_445_m = ca_445_m.step();
+        // newborn
+        assert_eq!(ca_445_m.cells[6][5][5].0, 5);
+        assert_eq!(ca_445_m.cells[6][5][6].0, 5);
+        // shouldn't be born
+        assert_eq!(ca_445_m.cells[6][5][7].0, 0);
+        // dying
+        assert_eq!(ca_445_m.cells[5][5][5].0, 4);
+        assert_eq!(ca_445_m.cells[5][5][6].0, 4);
+        assert_eq!(ca_445_m.cells[7][5][5].0, 4);
+        assert_eq!(ca_445_m.cells[7][5][6].0, 4);
     }
 }
